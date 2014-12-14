93668bf691440a21f81d88130dc328ccd9b287f2
Bastian Raschke <bastian.raschke@posteo.de>
2014-12-14 23:10:34 +0100

    Implemented arduhex decoding; Slim improvements, fixes

--- src/files/libraries/ArduKeyVerification.py
+++ src/files/libraries/ArduKeyVerification.py
@@ -17,9 +17,9 @@ from libraries.AESWrapper import AESWrapper
 from libraries.SQLiteWrapper import SQLiteWrapper
 
 
-class NoAPIKeyAvailableError(Exception):
+class NoAPIIdAvailableError(Exception):
     """
-    Dummy exception class for not available API key for Hmac signature check.
+    Dummy exception class for not available API id.
 
     """
 
@@ -45,7 +45,7 @@ class ArduKeyVerification(object):
     """
 
     __sharedSecret = ''
-    __response = {}
+    __response = {'otp': '', 'nonce': '', 'time': '', 'status': '', 'hmac': ''}
 
     def __init__(self, request):
         """
@@ -87,18 +87,48 @@ class ArduKeyVerification(object):
         ## Calculate HMAC of current response
         return hmac.new(sharedSecret, msg=payloadData, digestmod=hashlib.sha256).hexdigest()
 
-    def __convertModHex(self, modhexData):
+    def __decodeArduHex(self, data):
         """
-        Converts a modhex string to hexadecimal string.
+        Converts a arduhex input string to hexadecimal string.
 
-        @param string modhexData
-        The modhex string to convert.
+        @param string data
+        The arduhex string to convert.
 
         @return string
         """
 
-        ## TODO
-        return modhexData
+        ## Hexadecimal table
+        hexTable = '0123456789abcdef'
+
+        ## TODO: Own table
+        ## ArduKey transformation table
+        arduhexMappingTable = 'cbdefghijklnrtuv'
+
+        result = ''
+
+        for i in range(0, len(data)):
+
+            position = arduhexMappingTable.find(data[i])
+
+            ## Checks if character was found
+            if ( position == -1 ):
+                raise ValueError('The given input contains a non-valid character!')
+            else:
+                result += hexTable[position]
+
+        return result
+
+    def __calculateCRC16(self, data):
+        """
+        Calculates the CRC16 checksum for given data.
+
+        @param string data
+        The data used for calculation.
+
+        @return string
+        """
+
+        return 'TODO'
 
     def __validateRequest(self, request):
         """
@@ -118,82 +148,63 @@ class ArduKeyVerification(object):
             requestHmac = urllib.parse.quote(request['hmac'][0])
 
 
-
-
-
-
-
-            ## Get shared secret to verify request Hmac
-            SQLiteWrapper.getInstance().cursor.execute(
-                'SELECT secret FROM API WHERE id=?',
-                [
-                    apiId,
-                ]
-            )
-
-            rows = SQLiteWrapper.getInstance().cursor.fetchall()
-
-            if ( len(rows) > 0 ):
-                self.__sharedSecret = rows[0][0]
-            else:
-                raise NoAPIKeyAvailableError('No API key found in database!')
+            self.__response['otp'] = otp
+            self.__response['nonce'] = nonce
 
             request = {}
             request['otp'] = otp
             request['nonce'] = nonce
             request['apiId'] = apiId
 
+
+            ## Get shared secret of API Id to verify request
+            SQLiteWrapper.getInstance().cursor.execute(
+                'SELECT secret FROM API WHERE id = ?', [
+                apiId,
+            ])
+
+            rows = SQLiteWrapper.getInstance().cursor.fetchall()
+
+            if ( len(rows) > 0 ):
+                self.__sharedSecret = rows[0][0]
+            else:
+                raise NoAPIIdAvailableError('The API id was not found in database!')
+
+
+
             calculatedRequestHmac = self.__calculateHmac(request)
+            print('DEBUG: calculatedRequestHmac = ' + calculatedRequestHmac)
 
-            ## TODO
-            ## Compare Hmac via special method to prevent potential timing attacks
-            ## if ( hmac.compare_digest(requestHmac, calculatedRequestHmac) == False ):
-
-            ## TODO
-            print(calculatedRequestHmac)
-
+            ## Compare hashes
+            ## Unfortunatly the hmac.compare_digest() method is only available in Python 3.3+
             if ( requestHmac != calculatedRequestHmac ):
                 raise BadHmacSignatureError('The request Hmac signature is invalid!')
 
             ## Try to validate OTP
             if ( self.__validateOTP(otp) == True ):
-                self.__response['otp'] = otp
-                self.__response['nonce'] = nonce
                 self.__response['status'] = 'OK'
 
             else:
-                self.__response['otp'] = otp
-                self.__response['nonce'] = nonce
                 self.__response['status'] = 'INVALID_OTP'
 
-        ## The OTP has an invalid format
-        ## except ValueError:
-        ##     self.__response['otp'] = otp
-        ##     self.__response['nonce'] = nonce
-        ##     self.__response['status'] = 'CURRUPTED_OTP'
+        except ValueError:
+            ## The OTP has an invalid format
+            self.__response['status'] = 'CURRUPTED_OTP'
 
-        ## The API key was not found
-        except NoAPIKeyAvailableError:
-            self.__response['otp'] = otp
-            self.__response['nonce'] = nonce
-            self.__response['status'] = 'NO_APIKEY_AVAILABLE'
+        except NoAPIIdAvailableError:
+            ## The API id was not found
+            self.__response['status'] = 'API_ID_NOTFOUND'
 
-        ## The request Hmac signature is bad
         except BadHmacSignatureError:
-            self.__response['otp'] = otp
-            self.__response['nonce'] = nonce
+            ## The request Hmac signature is bad
             self.__response['status'] = 'INVALID_SIGNATURE'
 
-        ## Some parameters are not okay:
         except KeyError:
-            self.__response['otp'] = ''
-            self.__response['nonce'] = ''
+            ## Some parameters are not okay
             self.__response['status'] = 'MISSING_PARAMETER'
 
         ## General errors
         ## except:
-        ##     self.__response['otp'] = ''
-        ##     self.__response['nonce'] = ''
         ##     self.__response['status'] = 'UNKNOWN_ERROR'
 
     def __validateOTP(self, otp):
@@ -228,39 +239,40 @@ class ArduKeyVerification(object):
             raise ValueError('The OTP does not contain public id or token!')
 
         ## Convert public id and token to default hexadecimal string representation
-        publicId = self.__convertModHex(publicId)
-        encryptedToken = self.__convertModHex(encryptedToken)
-
+        publicId = self.__decodeArduHex(publicId)
+        encryptedToken = self.__decodeArduHex(encryptedToken)
 
         ## Get needed information from database
         SQLiteWrapper.getInstance().cursor.execute(
-            'SELECT publicid, secretid, counter, timestamp, aeskey, status FROM OTP WHERE publicid=?',
-            [
-                publicId,
-            ]
-        )
+            'SELECT secretid, counter, timestamp, aeskey, status FROM OTP WHERE publicid = ?', [
+            publicId,
+        ])
 
         rows = SQLiteWrapper.getInstance().cursor.fetchall()
 
         if ( len(rows) > 0 ):
             secretId = rows[0][0]
-            oldCounter = rows[0][1]
-            oldTimestamp = rows[0][2]
+            oldCounter = int(rows[0][1])
+            oldTimestamp = int(rows[0][2])
             aesKey = rows[0][3]
-            status = rows[0][4]
+            status = int(rows[0][4])
         else:
+            print('DEBUG: No OTP found in database')
             ## No OTP found in database
             return False
 
         ## Check status of ArduKey (maybe it is revoked)
-        if ( status != 1 ):
+        if ( status == 0 ):
+            print('DEBUG: OTP has been revoked!')
+            ## The ArduKey has been revoked!
             return False
 
-        aes = AESWrapper(aesKey)
-        rawToken = aes.decrypt(encryptedToken)
+        ## Decrypt encrypted token
+        rawToken = AESWrapper(aesKey).decrypt(encryptedToken)
 
-        ## TODO
-        ## check if aes error occured?
+        print('DEBUG: rawToken = ' + str(rawToken))
+
+        ## TODO: CRC16 check
 
         ## TODO
         ## Checks if database secretid matches to OTP value
